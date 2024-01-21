0466ba55fd784ab9e471c9264bd54d8a7718bffa
gleb <gleb@hiddenteam.ru>
2024-01-21 18:31:24 +0300

    Add logging

--- settings.py
+++ settings.py
@@ -1,5 +1,10 @@
 import dataclasses
+import logging
+import logging.config
+import os
+import sys
 
+import yaml
 from pydantic_settings import BaseSettings
 
 
@@ -19,6 +24,25 @@ class AppConfig:
     secret_key = "secrets"
     role = RoleConfig()
     database = Database()
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    logging_conf = base_dir + "/logging.yml"
 
 
 config_database = Database()
+
+
+# Logger config read
+if (
+    os.path.isfile(AppConfig.logging_conf)
+    and os.access(AppConfig.logging_conf, os.R_OK)
+):
+    lc_stream = open(AppConfig.logging_conf, "r")
+    lc_conf = yaml.load(lc_stream, Loader=yaml.FullLoader)
+    lc_stream.close()
+    logging.config.dictConfig(lc_conf)
+else:
+    print(
+        f"ERROR: logger config file '{AppConfig.logging_conf}' not exists or "
+        f"not readable\n"
+    )
+    sys.exit(1)
